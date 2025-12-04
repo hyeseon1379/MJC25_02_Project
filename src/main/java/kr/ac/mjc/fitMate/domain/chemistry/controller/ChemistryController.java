6ca900d1f629895ce45e822562916216e274814a
dasong <eoxnsvz88@naver.com>
2025-12-05 05:33:09 +0900

    궁합 분석 수정

--- src/main/java/kr/ac/mjc/fitMate/domain/chemistry/controller/ChemistryController.java
+++ src/main/java/kr/ac/mjc/fitMate/domain/chemistry/controller/ChemistryController.java
@@ -2,21 +2,17 @@ package kr.ac.mjc.fitMate.domain.chemistry.controller;
 
 import kr.ac.mjc.fitMate.ai.GeminiService;
 import kr.ac.mjc.fitMate.domain.chemistry.dto.*;
-import kr.ac.mjc.fitMate.domain.chemistry.service.ChemistryService;
-import jakarta.servlet.http.HttpSession;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import lombok.RequiredArgsConstructor;
 
 import java.util.Map;
 
+@RequiredArgsConstructor
 @Controller
-@RequestMapping("/api/chemistry")
-@CrossOrigin(origins = "*") // CORS 설정
+@RequestMapping("/chemistry")
 public class ChemistryController {
 
     private final GeminiService geminiService;
@@ -24,10 +20,6 @@ public class ChemistryController {
     @GetMapping
     public String asdf() {
         return "chemistry-form";
-    }
-    @Autowired
-    public ChemistryController(GeminiService geminiService) {
-        this.geminiService = geminiService;
     }
 
     @PostMapping("/analyze")
